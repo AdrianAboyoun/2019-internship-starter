a4c8e11e001511467f37776c1d24073637c3b0ae
AdrianAboyoun <adrian.aboyoun@gmail.com>
2019-04-08 16:36:22 +0100

    final version: 3 digits

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,8 +1,8 @@
 function getSequence(n) {
-  var arr = [1, 1];
+  var arr = [1, 1]; //seed array with two initial values
   var i;
   for (i = 0; i < n - 2; i++) {
-    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]); //generate sequence from seeds to desired value
   }
   return arr;
 }
--- src/tableFormatter.js
+++ src/tableFormatter.js
@@ -1,61 +1,47 @@
 function format(table) {
+  const double = 10;
+  const triple = 100;
   var most = 0;
-  //var least;
   var i;
   var j;
   for (i = 0; i < table.length; i++) {
     for (j = 1; j < table[i].length; j++) {
-      if (table[i][j] > most) {
+      if (table[i][j] > most)
         most = table[i][j];
-      }
     }
-  }
+  } //finds greatest value in table so it can be formatted accordingly
   for (i = 0; i < table.length; i++) {
-    if (most > 9) {
+    if (most >= double)
       table[i][0] = " " + table[i][0].toString();
-    }
+      //adds leading space to each row for two and three digit tables
+      //(not sure why, but matches tests)
     for (j = 1; j < table[i].length; j++) {
-      if (most < 10) {
-        if (table[i][j-1] < 10 && table[i][j] < 10) {
-          table[i][j-1] = table[i][j-1].toString() + " ";
-        }
-      }
-      else if (most >= 10 && most < 100) {
-        if (table[i][j-1] < 10 && table[i][j] >= 10 && table[i][j] < 100) {
+      if (most < double)
+        table[i][j-1] = table[i][j-1].toString() + " ";
+     // (block above) formats single-digit tables
+      else if (most >= double && most < triple) {
+        if (table[i][j-1] < triple && table[i][j] >= double && table[i][j] < triple) {
           table[i][j-1] = table[i][j-1].toString() + " ";
           table[i][j] = table[i][j].toString();
         }
-        else if (table[i][j-1] >= 10 && table[i][j-1] < 100 && table[i][j] >= 10 && table[i][j] < 100) {
-          table[i][j-1] = table[i][j-1].toString() + " ";
-          table[i][j] = table[i][j].toString();
-        }
-        else {
+        else
           table[i][j-1] = table[i][j-1].toString() + "  ";
-        }
-      }
+      } //formats two-digit tables
       else {
-        if (table[i][j-1] < 10) {
+        if (table[i][j-1] < double)
           table[i][j-1] = " " + table[i][j-1].toString() + "  ";
-        }
-        else if (table[i][j-1] < 100 && table[i][j] < 100) {
+        else if (table[i][j-1] < triple && table[i][j] < triple)
           table[i][j-1] = table[i][j-1].toString() + "  ";
-          table[i][j] = table[i][j].toString();
-        }
-        else if (table[i][j-1] < 100 && table[i][j] >= 100) {
+        else if (table[i][j] >= triple)
           table[i][j-1] = table[i][j-1].toString() + " ";
-          table[i][j] = table[i][j].toString();
-        }
-        else if (table[i][j-1] >= 100 && table[i][j] >= 100) {
-          table[i][j-1] = table[i][j-1].toString() + " ";
-          table[i][j] = table[i][j].toString();
-        }
-      }
+        table[i][j] = table[i][j].toString();
+      }//formats three-digit tables
     }
   }
   const tableText = table.map(function(row) {
       return row.join("");
   });
-
+  //converts array of formatted strings into a single string to return.
   return "\n" + tableText.join("\n") + "\n";
 }
 
